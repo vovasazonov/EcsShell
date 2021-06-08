46c1304ad386ad5265d01467c462e46fa685ab71
Vladimir Sazonov <vova.sazonovvv@gmail.com>
2021-06-08 14:39:50 +0300

    chg: implement package

--- EcsUnityProject/Assets/Ecs/ListenerComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Ecs
-{
-    internal struct ListenerComponent<TComponent> where TComponent : struct
-    {
-        public List<IComponentListener<TComponent>> Value;
-    }
-}
---
+++ EcsUnityProject/Packages/EcsShell/Ecs/ListenerComponent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Ecs
+{
+    public struct ListenerComponent<TComponent> where TComponent : struct
+    {
+        public List<IComponentListener<TComponent>> Value;
+    }
+}
