68271c55a961a32bcde2f33ba3015afc9588e9ca
Vladimir Sazonov <vova.sazonovvv@gmail.com>
2021-06-08 11:31:01 +0300

    fix: duplicate execute systems

--- EcsUnityProject/Assets/Scripts/Ecs/EntitasExtension/Systems.cs
+++ EcsUnityProject/Assets/Scripts/Ecs/EntitasExtension/Systems.cs
@@ -32,30 +32,22 @@ namespace Ecs.EntitasExtension
 
         public void Add<T>(T system) where T : ISystem
         {
-            if (system is IReactSystem react)
+            if (system is IUpdateSystem update)
             {
-                Entitas.ICollector<Entity> collector;
-                Entitas.IMatcher<Entity> matcher = ((Matcher) react.Matcher.Invoke(_world.Matcher)).GetMatcher();
-                if (system is IEventSystem)
+                if (system is IReactSystem react)
                 {
-                    collector = Entitas.CollectorContextExtension.CreateCollector(_world.Context, Entitas.TriggerOnEventMatcherExtension.Added(matcher));
+                    var collector = CreateCollector(system, react);
+                    _feature.Add(new ReactiveEntitasSystem<T>(react, collector));
                 }
                 else
                 {
-                    collector = _world.Context.CreateCollector(matcher);
+                    _feature.Add(new ExecuteEntitasSystem<T>(update));
                 }
-
-                _feature.Add(new ReactiveEntitasSystem<T>(react, collector));
             }
 
             if (system is IInitializeSystem initialize)
             {
                 _feature.Add(new InitializeEntitasSystem<T>(initialize));
-            }
-
-            if (system is IUpdateSystem update)
-            {
-                _feature.Add(new ExecuteEntitasSystem<T>(update));
             }
 
             if (system is ILateUpdateSystem lateUpdate)
@@ -67,6 +59,22 @@ namespace Ecs.EntitasExtension
             {
                 _feature.Add(new TearDownEntitasSystem<T>(destroy));
             }
+        }
+
+        private Entitas.ICollector<Entity> CreateCollector<T>(T system, IReactSystem react) where T : ISystem
+        {
+            Entitas.ICollector<Entity> collector;
+            Entitas.IMatcher<Entity> matcher = ((Matcher) react.Matcher.Invoke(_world.Matcher)).GetMatcher();
+            if (system is IEventSystem)
+            {
+                collector = Entitas.CollectorContextExtension.CreateCollector(_world.Context, Entitas.TriggerOnEventMatcherExtension.Added(matcher));
+            }
+            else
+            {
+                collector = _world.Context.CreateCollector(matcher);
+            }
+
+            return collector;
         }
     }
 }
